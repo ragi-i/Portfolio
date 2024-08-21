23d9a14b654773b3bb258fa28b523dbd4cc74f3d
Ragnee <ragnee2001@gmail.com>
2024-08-21 19:47:32 +0530

    code committed

---
+++ src/constants/index.js
@@ -0,0 +1,370 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  // redux,
+  // tailwind,
+  nodejs,
+  mongodb,
+  git,
+  // figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  // threejs,
+  Angular,
+  jenkins,
+  Postman,
+  github,
+  lumiq,
+  // portfolioInsurance,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Programmer",
+    icon: mobile,
+  },
+  {
+    title: "Badminton Player",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  // {
+  //   name: "Tailwind CSS",
+  //   icon: tailwind,
+  // },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "github",
+    icon: github,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "Angular",
+    icon: Angular,
+  },
+  {
+    name: "jenkins",
+    icon: jenkins,
+  },
+  {
+    name: "Postman",
+    icon: Postman,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "LUMIQ",
+    icon: lumiq,
+    iconBg: "#383E56",
+    date: "January 2024 - June 2024",
+    points: [
+      "Contributed to full-stack apps for Insurance-Provider Team, creating APIs and optimizing backend and frontend.",
+      "Led Angular project, boosting user engagement by 25 Percentage with improved features and API integrations.",
+      "Contributed to Application, DevOps, Data Science, and Business Intelligence domains during bootcamp projects.",
+      "Spearheaded end-to-end development efforts, driving company growth by delivering high-quality web solutions.",
+    ],
+  },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+// const testimonials = [
+//   {
+//     testimonial:
+//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+//     name: "Sara Lee",
+//     designation: "CFO",
+//     company: "Acme Co",
+//     image: "https://randomuser.me/api/portraits/women/4.jpg",
+//   },
+//   {
+//     testimonial:
+//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+//     name: "Chris Brown",
+//     designation: "COO",
+//     company: "DEF Corp",
+//     image: "https://randomuser.me/api/portraits/men/5.jpg",
+//   },
+//   {
+//     testimonial:
+//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+//     name: "Lisa Wang",
+//     designation: "CTO",
+//     company: "456 Enterprises",
+//     image: "https://randomuser.me/api/portraits/women/6.jpg",
+//   },
+// ];
+
+const testimonials = [
+
+   {
+    testimonial:
+     " Solving Bugs :- Tackling and resolving bugs in websites and applications not only challenges me intellectually but also enhances my technical skills. It has become a crucial aspect of my growth as a Developer.",
+ //  name: "Chris Brown",
+ //  designation: "COO",
+ //  company: "DEF Corp",
+    image: "https://img.freepik.com/premium-vector/developer-team-developing-maintaining-web-site-solving-error-bugs-concept_251235-393.jpg",
+  },
+  {
+    testimonial:
+     "Playing badminton :— It is a passion that embodies my love for athleticism.Playing badminton not only challenges me physically but also enhances my mental agility.It has become a crucial outlet for stress relief.",
+ //  name: "Chris Brown",
+ //  designation: "COO",
+ //  company: "DEF Corp",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL68_ilwF0Hgws_WO47LyVQ7YJ5UJJjWv7Q&usqp=CAU",
+  },
+
+ // {
+   
+  // testimonial:
+   
+ // "  I have a deep appreciation for Korean dramas, also known as K-dramas. My passion for K-dramas may be seen as a leisurely hobby, it has also enriched my life and contributed to my personal and professional growth in unexpected ways."
+    // "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  // name: "Sara Lee",
+  // designation: "CFO",
+  // company: "Acme Co",
+  // image: "https://randomuser.me/api/portraits/women/4.jpg",
+ //},
+  {
+  testimonial:
+     "Volunteering :- It has given me the opportunity to develop a deeper sense of empathy,social responsibility.It has provided me satisfaction of helping others and allowed me to develop valuable skills.",
+ //  name: "Lisa Wang",
+ //  designation: "CTO
+ //  company: "456 Enterprises",
+  // image: "https://randomuser.me/api/portraits/women/6.jpg",
+  image :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ59e0aNi4fhMuU806ulPymnYdkSZaswjl5JA&usqp=CAU",
+  },
+];
+
+const projects = [
+  {
+    name: "Insurance App",
+    description:
+      // "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "A web-based platform that allows users to store, purchase, and claim various insurance policies, providing a seamless and efficient solution for managing insurance needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "node js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "express js",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/ragi-i/Claims_Management_",
+    live_project_link: "https://insuranceapp-ragnees-projects.vercel.app/",
+  },
+  {
+    name: "Status Code Images",
+    description:
+      "A full-stack project allowing developers to search for HTTP status codes, view related images, and save them to their account for future reference.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "node js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "express js",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/ragi-i/StatusCode_Images",
+    live_project_link: "https://status-code-images.vercel.app/",
+  },
+   {
+    name: "Blood Bank Record",
+    description:
+      // "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "A blood bank system that tracks blood groups, stores donor details, and manages medical personnel information for efficient donation and inventory management.",
+      tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "node js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "express js",
+        color: "violet-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/ragi-i/Blood_Bank_Record",
+    live_project_link: "https://blood-bank-app-xwwv.onrender.com/",
+  },
+  // {
+  //   name: "Blood Bank Record",
+  //   description:
+  //     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+  //   tags: [
+  //     {
+  //       name: "react",
+  //       color: "blue-text-gradient",
+  //     },
+  //     {
+  //       name: "mongodb",
+  //       color: "green-text-gradient",
+  //     },
+  //     {
+  //       name: "node js",
+  //       color: "pink-text-gradient",
+  //     },
+  //     {
+  //       name: "express js",
+  //       color: "pink-text-gradient",
+  //     },
+  //   ],
+  //   image: tripguide,
+  //   source_code_link: "https://github.com/",
+  // },
+];
+
+export { services, technologies, experiences, testimonials, projects };
